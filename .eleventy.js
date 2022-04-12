147aa2eb0cded0d50d594c865e961228abcc8bb1
Matt Henry <hey@matthenry.fyi>
2022-04-12 10:11:05 -0400

    Add GSA logo

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const yaml = require('js-yaml');
 module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
 
+  eleventyConfig.addPassthroughCopy('assets/img');
   return {
     dir: {
       input: 'src',
