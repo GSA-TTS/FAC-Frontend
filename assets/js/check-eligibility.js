e582cbcbca26cb7ed9f50302839def43f5d697b7
Matt Henry <matt.henry@gsa.gov>
2022-04-22 15:14:06 -0400

    Validate entity type

--- assets/js/check-eligibility.js
+++ assets/js/check-eligibility.js
@@ -21,15 +21,61 @@
     return Object.fromEntries(formData);
   }
 
+  function isValidEntity({name, id}) {
+    const INVALID_ENTITY_TYPES = {
+      user_provided_organization_type: ["entity-none"],
+      met_spending_threshold: ["spend-no"],
+      is_usa_based: ["us-no"]
+    }
+
+    return !INVALID_ENTITY_TYPES[name].includes(id);
+  }
+
+  function resetErrorStates(el) {
+    const inputsWithErrors = Array.from(el.querySelectorAll('.usa-radio--error'));
+    inputsWithErrors.forEach(i => i.classList.remove('usa-radio--error'));
+  }
+
+  function validateEntity(entity) {
+    const radioEl = entity.parentElement;
+    const fieldsetEl = radioEl.parentElement;
+    resetErrorStates(fieldsetEl);
+
+    if (!isValidEntity(entity) && entity.checked) {
+      radioEl.classList.add('usa-radio--error');
+    };
+  }
+
+  function runAllValidations() {
+    const inputs = Array.from(document.querySelectorAll('.question input'));
+    const validations = [validateEntity];
+
+    inputs.forEach(input => {
+      validations.forEach(validation => validation(input));
+    })
+  }
+
+  function allResponsesValid() {
+    const inputsWithErrors = document.querySelectorAll('[class *="--error"]');
+    return inputsWithErrors.length === 0;
+  }
+
   function attachEventHandlers() {
     FORM.addEventListener('submit', (e) => {
       e.preventDefault();
+      if (!allResponsesValid()) return;
       submitForm();
     })
+
+    const questions = Array.from(document.querySelectorAll('.question'));
+    questions.forEach(q => {
+      q.addEventListener('change', e => validateEntity(e.target));
+    });
   }
 
   function init() {
     attachEventHandlers();
+    runAllValidations(); // Run these on load in case the user refreshed
   }
 
   init();
