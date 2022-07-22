35d8e33dab75ed4d509eb31c88e67a74dd1aa0cc
Rachel Schneiderman <rschneiderman379@gmail.com>
2022-07-22 15:36:09 -0700

    391: Add submissions data script, refactor template variable naming

---
+++ src/_data/submissions.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    reportId: '2022MRU0024654156',
+    status: 'Needs certification',
+    auditeeUEI: 'NUDGYLBB4S99',
+    auditeeName: 'NEW YORK, STATE OF',
+    fiscalYearEndDate: '6/30/2021',
+  },
+  {
+    reportId: '2022SPN00246541312',
+    status: 'Needs certification',
+    auditeeUEI: 'NLCZMHXJ39H7',
+    auditeeName: 'UNITEDHEALTH GROUP INCORPORATED',
+    fiscalYearEndDate: '6/30/2021',
+  },
+  {
+    reportId: '2022AEB00125460479',
+    status: 'In progress',
+    auditeeUEI: 'G6JLG3FANUA9',
+    auditeeName: 'DEPARTMENT OF PUBLIC SAFETY',
+    fiscalYearEndDate: '3/31/2021',
+  },
+  {
+    reportId: '2022LZJ00102340508',
+    status: 'In progress',
+    auditeeUEI: 'GQ46SB5L2HK4',
+    auditeeName: 'PUERTO RICO, COMMONWEALTH OF',
+    fiscalYearEndDate: '3/31/2021',
+  },
+  {
+    reportId: '2022CWB00101268655',
+    status: 'In progress',
+    auditeeUEI: 'CVBGYLBB4S77',
+    auditeeName: 'CHOCTAW NATION OF OKLAHOMA',
+    fiscalYearEndDate: '6/30/2021',
+  },
+];
