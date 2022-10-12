1cdaffb7f4bc2f64b266c7a641e9681b08037d98
Jason Nakai <106776019+jasnakai@users.noreply.github.com>
2022-10-12 11:19:08 -0600

    Fixing Cypress tests.

--- cypress/e2e/check-submissions.cy.js
+++ cypress/e2e/check-submissions.cy.js
@@ -7,80 +7,14 @@ describe('Display my audit submissions', () => {
     it('does not display the submissions table', () => {
       cy.intercept(
         {
-          method: 'GET', // Route all GET requests
-          url: 'https://fac-dev.app.cloud.gov/submissions', // that have a URL that matches '/users/*'
+          method: 'GET',
+          url: 'https://fac-dev.app.cloud.gov/submissions',
         },
-        {
-          response: [
-            {
-              report_id: '2021FQF0001000003',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: 'ibm',
-            },
-            {
-              report_id: '20215L30001000005',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: 'SubTest1',
-            },
-            {
-              report_id: '2021JG70001000010',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74xW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: 'INTERNATIONAL BUSINESS MACHINES CORPORATION',
-            },
-            {
-              report_id: '2021E650001000011',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74xW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: 'asd',
-            },
-            {
-              report_id: '2021MCM0001000013',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: null,
-            },
-            {
-              report_id: '2021RFJ0001000014',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: null,
-            },
-            {
-              report_id: '2021WEU0001000015',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: null,
-            },
-            {
-              report_id: '2021BGX0001000016',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2022-01-01',
-              auditee_name: null,
-            },
-            {
-              report_id: '20216KN0001000017',
-              submission_status: 'in_progress',
-              auditee_uei: 'ZQGGHJH74DW7',
-              auditee_fiscal_period_end: '2021-11-01',
-              auditee_name: null,
-            },
-          ],
-        }
+        []
       ).as('submissionsRequest');
 
       cy.wait('@submissionsRequest').then((interception) => {
-        assert.isNotNull(interception.response.body, '1st API call has data');
+        assert.isNotNull(interception.response.body, 'has data');
       });
     });
   });
